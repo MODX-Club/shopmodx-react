555123185bf721036ab39e0bf2007dd96cffe33b
Fi1osof <n.lanets@modxclub.ru>
2017-12-11 03:38:36 +0300

    v1.0.0

---
+++ components/Pages/MainPage/index.js
@@ -0,0 +1,13 @@
+
+
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import DefaultPage from '../DefaultPage';
+
+export default class MainPage extends DefaultPage{
+	
+ 
+
+}
